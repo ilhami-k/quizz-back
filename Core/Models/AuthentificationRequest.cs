f33179366d32ac792909a9401b5f7a47f4cc65ff
Skrinja <clement.simon@hainaut-promsoc.be>
2025-05-28 17:59:57 +0200

    ADD : Class AuthentificationRequest et RegisterRequest pour la connexion et inscription future

---
+++ Core/Models/AuthentificationRequest.cs
@@ -0,0 +1,8 @@
+
+namespace Core.Models;
+
+public class AuthentificationRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
